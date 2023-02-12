92d5aa9d2c5b341f1671cb06b2ee8c9a637e2fad
my <my@AIWS-PC>
2023-02-12 13:19:42 +0530

    commit

---
+++ website-pubg/src/main/java/com/websitepubg/websitepubg/WebsitePubgApplication.java
@@ -0,0 +1,13 @@
+package com.websitepubg.websitepubg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsitePubgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsitePubgApplication.class, args);
+	}
+
+}
---
+++ website-pubg/src/test/java/com/websitepubg/websitepubg/WebsitePubgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websitepubg.websitepubg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsitePubgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
